1147f50951530febba50dd5bd08866b5cafcbe53
Brad Montgomery <brad@bradmontgomery.net>
2013-01-04 11:21:09 -0600

    try not to break setup

--- rainbowtests/__init__.py
+++ rainbowtests/__init__.py
@@ -1,3 +1,6 @@
-from .tests import RainbowTestSuiteRunner
+try:
+    from .tests import RainbowTestSuiteRunner
+except ImportError:
+    pass  # this isn't going to work while installing, but that's ok (i think)
 
 __version__ = '0.1.1'
