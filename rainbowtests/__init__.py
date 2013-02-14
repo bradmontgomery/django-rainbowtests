f98a21d592f6f010150b1e9cd1383249863be3a9
Brad Montgomery <brad@bradmontgomery.net>
2013-02-13 21:55:48 -0600

    sigh. pip installing this breaks if django's not yet configured

--- rainbowtests/__init__.py
+++ rainbowtests/__init__.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ImproperlyConfigured
+
 try:
     from .tests import RainbowTestSuiteRunner
-except ImportError:
+except (ImportError, ImproperlyConfigured):
     pass  # this isn't going to work while installing, but that's ok (i think)
 
 __version__ = '0.1.1'
