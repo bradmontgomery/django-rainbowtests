815b210bd24dea818c8d42560cc0410e4e909bb9
Brad Montgomery <brad@bradmontgomery.net>
2014-07-19 11:52:43 -0500

    we don't need all this

--- rainbowtests/__init__.py
+++ rainbowtests/__init__.py
@@ -1,8 +1,1 @@
-from django.core.exceptions import ImproperlyConfigured
-
-try:
-    from .tests import RainbowTestSuiteRunner
-except (ImportError, ImproperlyConfigured):
-    pass  # this isn't going to work while installing, but that's ok (i think)
-
-__version__ = '0.2.0'
+__version__ = '0.2.1'
