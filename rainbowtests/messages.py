c59586e86661561334b3cb6f76ee08538c3e46dc
Brad Montgomery <brad@bradmontgomery.net>
2014-07-19 11:53:46 -0500

    clearer import; pep8 E122

--- rainbowtests/messages.py
+++ rainbowtests/messages.py
@@ -1,67 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import colors
 import random
+from rainbowtests import colors
+
 
 happy_messages = []
 sad_messages = []
 
-happy_messages.append(
-"""
+happy_messages.append("""
 
         (づ｡◕‿‿◕｡)づ・。*。✧・゜・。
         ✧。*・゜゜・✧。・゜゜SUCCESS!!・。
         *。・゜*✧✿。*。✧・゜゜・✧。✧。*・
 
-"""
-)
+""")
 
-happy_messages.append(
-"""
+happy_messages.append("""
 
         (☞ﾟ∀ﾟ)☞ You da Man!
         All tests passed.
 
-"""
-)
+""")
 
-happy_messages.append(
-"""
+happy_messages.append("""
 
         ヽ(´▽`)/ Hallelujah!
         Praise FSM. Success.
 
-"""
-)
+""")
 
-happy_messages.append(
-"""
+happy_messages.append("""
 
         (｡♥‿♥｡) ・。♥。✧・゜・✿
         ✧。*・Your code loves you.✧♥
         ✿。・ All tests passed!✿。*
         ✧・゜♥・✧。♥。*・。♥。✧・♥・✿
-"""
-)
+""")
 
-sad_messages.append(
-"""
+sad_messages.append("""
 
         （╯°□°）╯︵ ┻━┻
         AAAAAAAAAARGGGGGGGGGG!!!
 
-"""
-)
+""")
 
-sad_messages.append(
-"""
+sad_messages.append("""
 
         ლ(ಠ益ಠლ)
         Bloody tests failed AGAIN!
 
-"""
-)
+""")
 
 
 def random_happy():
